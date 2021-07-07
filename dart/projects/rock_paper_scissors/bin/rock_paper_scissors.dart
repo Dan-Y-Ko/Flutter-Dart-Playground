625c7b46a83629358f7e1c294a48da29cf3b9064
Dan <danko56666@gmail.com>
2021-07-07 10:26:51 -0400

    implement game logic

--- dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
+++ dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
@@ -1,6 +1,12 @@
 import 'dart:io';
 
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
+
 void main() {
+  final rng = Random();
+
   // while true
   while (true) {
     // show prompt
@@ -11,7 +17,31 @@ void main() {
 
     // if input is valid move("r", "p", "s")
     if (input == 'r' || input == 'p' || input == 's') {
-      print('Selected: $input');
+      var playerMove;
+
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove == Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+
+      print('you played: $playerMove');
+      print('ai played: $aiMove');
+
+      if (playerMove == aiMove) {
+        print('it\'s a draw');
+      } else if (playerMove == Move.rock && aiMove == Move.scissors ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.scissors && aiMove == Move.paper) {
+        print('you win');
+      } else {
+        print('you lose');
+      }
 
       // else if input is q
     } else if (input == 'q') {
