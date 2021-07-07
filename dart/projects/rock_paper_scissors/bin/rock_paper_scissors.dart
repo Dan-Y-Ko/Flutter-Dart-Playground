e818c7db26be0f1b059482bfc313547b0146f9a1
Dan <danko56666@gmail.com>
2021-07-07 10:11:32 -0400

    utilizie dart io library to read user input

--- dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
+++ dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 void main() {
   // show prompt
+  stdout.write('rock, paper, or scissors? (r/p/s) ');
 
   // read user input from console
+  final input = stdin.readLineSync();
+  print(input);
 
   // if input is valid move("r", "p", "s")
 
