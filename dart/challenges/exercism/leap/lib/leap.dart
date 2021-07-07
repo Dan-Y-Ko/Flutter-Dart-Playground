e6d8b292b9ebfddd1fc9aedbd04b60596dc606dd
Dan <danko56666@gmail.com>
2021-07-07 04:39:56 -0400

    leap year challenge

---
+++ dart/challenges/exercism/leap/lib/leap.dart
@@ -0,0 +1,12 @@
+class Leap {
+  bool leapYear(int year) {
+    if (year % 4 == 0) {
+      if ((year % 100 != 0 && year % 400 != 0) ||
+          (year % 100 == 0 && year % 400 == 0)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
