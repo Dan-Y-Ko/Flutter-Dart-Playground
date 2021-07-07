d4d2b624cd43995658513ea91e982378f1517177
Dan <danko56666@gmail.com>
2021-07-07 10:08:26 -0400

    write pseudocode

--- dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
+++ dart/projects/rock_paper_scissors/bin/rock_paper_scissors.dart
@@ -1,3 +1,21 @@
 void main() {
-  print('hello');
+  // show prompt
+
+  // read user input from console
+
+  // if input is valid move("r", "p", "s")
+
+  // choose the ai move at random
+
+  // compare the player move with the ai move
+
+  // show the result
+
+  // else if input is q
+
+  // quit the program
+
+  // else
+
+  // invalid input
 }
