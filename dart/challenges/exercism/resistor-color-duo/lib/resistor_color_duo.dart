90e2806017ecf30663158e3726aeb0c3494a69e9
Dan <danko56666@gmail.com>
2021-07-07 03:54:23 -0400

    add shorter solution

--- dart/challenges/exercism/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/challenges/exercism/resistor-color-duo/lib/resistor_color_duo.dart
@@ -26,3 +26,20 @@ class ResistorColorDuo {
     return int.parse(output.join());
   }
 }
+
+// class ResistorColorDuo {
+//   static const _colors = {
+//     'black': 0,
+//     'brown': 1,
+//     'red': 2,
+//     'orange': 3,
+//     'yellow': 4,
+//     'green': 5,
+//     'blue': 6,
+//     'violet': 7,
+//     'grey': 8,
+//     'white': 9,
+//   };
+
+//   int value(List<String> list) => _colors[list[0]]! * 10 + _colors[list[1]]!;
+// }
