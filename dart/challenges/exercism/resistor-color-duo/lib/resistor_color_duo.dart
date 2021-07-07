22fce41f2fad6439d6b90bbeba0a43672b00ea16
Dan <danko56666@gmail.com>
2021-07-07 03:47:14 -0400

    resistor_color_duo challenge

---
+++ dart/challenges/exercism/resistor-color-duo/lib/resistor_color_duo.dart
@@ -0,0 +1,28 @@
+class ResistorColorDuo {
+  static const _colors = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+  };
+
+  int value(List<String> input) {
+    List<String> output = [];
+
+    for (var i = 0; i < input.length; i++) {
+      if (i < 2) {
+        output.add(_colors[input[i]].toString());
+      }
+
+      continue;
+    }
+
+    return int.parse(output.join());
+  }
+}
