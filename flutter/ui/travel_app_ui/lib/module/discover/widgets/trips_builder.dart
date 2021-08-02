a31607a5cea9127136a5b4da8a390d1f281ebd37
Dan Ko <danko56666@gmail.com>
2021-08-02 00:38:33 -0400

    add text to card

--- flutter/ui/travel_app_ui/lib/module/discover/widgets/trips_builder.dart
+++ flutter/ui/travel_app_ui/lib/module/discover/widgets/trips_builder.dart
@@ -49,7 +49,39 @@ class TripsBuilder extends StatelessWidget {
                     ),
                   ),
                 ),
-                Text('asdasd'),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 23.0),
+                    child: Text(
+                      trips[index].title,
+                      style: TextStyle(
+                        fontFamily: 'DMSans700',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 4.0,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 23.0),
+                    child: Text(
+                      trips[index].caption,
+                      style: TextStyle(
+                        fontFamily: 'DMSans500',
+                        fontSize: 12.0,
+                        color: Color(0xFFACB1C2),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           );
