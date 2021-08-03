d05b95f8e979f0e92382ecbb6a304333fff54415
Dan Ko <danko56666@gmail.com>
2021-08-03 01:43:01 -0400

    split up widgets

--- flutter/ui/travel_app_ui/lib/module/discover/widgets/trip-detail/gallery/gallery_container.dart
+++ flutter/ui/travel_app_ui/lib/module/discover/widgets/trip-detail/gallery/gallery_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../models/gallery.dart';
+import 'gallery_list.dart';
 
 class GalleryContainer extends StatelessWidget {
   const GalleryContainer({Key? key}) : super(key: key);
@@ -22,53 +22,7 @@ class GalleryContainer extends StatelessWidget {
         SizedBox(
           height: 10.0,
         ),
-        Container(
-          margin: EdgeInsets.only(bottom: 36.0),
-          child: Row(
-            children: [
-              for (var i = 0; i < gallery.length; i++)
-                i == 3
-                    ? Container(
-                        margin: EdgeInsets.only(right: 5.0),
-                        height: 75.0,
-                        width: 80.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: AssetImage(gallery[i].imageUrl),
-                            fit: BoxFit.cover,
-                            colorFilter: ColorFilter.mode(
-                              Color.fromRGBO(34, 34, 34, 0.6),
-                              BlendMode.darken,
-                            ),
-                          ),
-                        ),
-                        child: Center(
-                          child: Text(
-                            '12+',
-                            style: TextStyle(
-                              color: Color(0xFFFFFFFF),
-                              fontFamily: 'Poppins600',
-                              fontSize: 20.0,
-                            ),
-                          ),
-                        ),
-                      )
-                    : Container(
-                        margin: EdgeInsets.only(right: 5.0),
-                        height: 75.0,
-                        width: 80.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            image: AssetImage(gallery[i].imageUrl),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-            ],
-          ),
-        )
+        GalleryList(),
       ],
     );
   }
