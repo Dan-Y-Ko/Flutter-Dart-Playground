be29b744ac613d159a34e3dae0b9649b6607cb97
Dan Ko <danko56666@gmail.com>
2021-08-03 00:58:27 -0400

    add ripple effect to reservation button

--- flutter/ui/travel_app_ui/lib/module/discover/widgets/trip-detail/reservation_button.dart
+++ flutter/ui/travel_app_ui/lib/module/discover/widgets/trip-detail/reservation_button.dart
@@ -6,31 +6,52 @@ class ReservationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(24.0, 0, 24.0, 33.0),
-      height: 74.0,
-      width: 366.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(24.0),
-        color: Color(0xFF222222),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SizedBox(),
-          Text(
-            'Book Now',
-            style: TextStyle(
-              color: Color(0xFFFFFFFF),
-              fontFamily: 'DMSans500',
-              fontSize: 20.0,
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(24.0, 0, 24.0, 33.0),
+          height: 74.0,
+          width: 366.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24.0),
+            color: Color(0xFF222222),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(),
+              Text(
+                'Book Now',
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontFamily: 'DMSans500',
+                  fontSize: 20.0,
+                ),
+              ),
+              SvgPicture.asset(
+                'assets/images/arrow-right-white.svg',
+              ),
+            ],
+          ),
+        ),
+        Positioned.fill(
+          left: 24.0,
+          right: 24.0,
+          bottom: 33.0,
+          child: Material(
+            borderRadius: BorderRadius.all(
+              Radius.circular(
+                24.0,
+              ),
+            ),
+            clipBehavior: Clip.hardEdge,
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
             ),
           ),
-          SvgPicture.asset(
-            'assets/images/arrow-right-white.svg',
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
