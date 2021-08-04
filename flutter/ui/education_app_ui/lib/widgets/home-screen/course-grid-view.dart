14ef2bce1dc4faf53611529d180d352b0f710ef0
Dan Ko <danko56666@gmail.com>
2021-08-04 05:30:41 -0400

    create course card widget to take in different values

--- flutter/ui/education_app_ui/lib/widgets/home-screen/course-grid-view.dart
+++ flutter/ui/education_app_ui/lib/widgets/home-screen/course-grid-view.dart
@@ -14,77 +14,134 @@ class CourseGridView extends StatelessWidget {
         mainAxisSpacing: 16.0,
         crossAxisCount: 2,
         children: <Widget>[
-          Container(
-            height: 185.0,
-            width: 156.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Color(0xFFDBFFD9),
-            ),
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 15.0, 16.0, 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Design',
-                    style: TextStyle(
-                      color: Color(0xFF4D2C5E),
-                      fontFamily: 'Inter700',
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 6.0,
-                  ),
-                  Text(
-                    '54 Lectures',
-                    style: TextStyle(
-                      color: Color(0xFF575E83),
-                      fontFamily: 'Inter400',
-                      fontSize: 12.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Center(
-                    child: Image.asset(
-                      'assets/images/Code_perspective.png',
-                      height: 70.0,
-                      width: 70.0,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      SvgPicture.asset('assets/images/fa-solid_user.svg'),
-                      Text(
-                        '1.2k',
-                        style: TextStyle(
-                          color: Color(0xFF575E83),
-                          fontFamily: 'Inter400',
-                          fontSize: 12.0,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 63.0,
-                      ),
-                      SvgPicture.asset('assets/images/Star.svg'),
-                      Text(
-                        '1.2k',
-                        style: TextStyle(
-                          color: Color(0xFF575E83),
-                          fontFamily: 'Inter400',
-                          fontSize: 12.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+          CourseCard(
+            title: 'Design',
+            numLecture: 54,
+            imageUrl: 'assets/images/Code_perspective.png',
+            numStudents: '1.2k',
+            rating: 4.5,
+            color: 'FEE2DE',
+          ),
+          CourseCard(
+            title: 'Design',
+            numLecture: 54,
+            imageUrl: 'assets/images/Code_perspective.png',
+            numStudents: '1.2k',
+            rating: 4.5,
+            color: 'FEE2DE',
+          ),
+          CourseCard(
+            title: 'Design',
+            numLecture: 54,
+            imageUrl: 'assets/images/Code_perspective.png',
+            numStudents: '1.2k',
+            rating: 4.5,
+            color: 'FEE2DE',
+          ),
+          CourseCard(
+            title: 'Design',
+            numLecture: 54,
+            imageUrl: 'assets/images/Code_perspective.png',
+            numStudents: '1.2k',
+            rating: 4.5,
+            color: 'FEE2DE',
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CourseCard extends StatelessWidget {
+  const CourseCard({
+    Key? key,
+    required this.title,
+    required this.numLecture,
+    required this.imageUrl,
+    required this.numStudents,
+    required this.rating,
+    required this.color,
+  }) : super(key: key);
+
+  final String title;
+  final int numLecture;
+  final String imageUrl;
+  final String numStudents;
+  final double rating;
+  final String color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 185.0,
+      width: 156.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Color(
+          int.parse('0xFF$color'),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(16.0, 15.0, 16.0, 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: Color(0xFF4D2C5E),
+                fontFamily: 'Inter700',
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              '$numLecture Lectures',
+              style: TextStyle(
+                color: Color(0xFF575E83),
+                fontFamily: 'Inter400',
+                fontSize: 12.0,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Center(
+              child: Image.asset(
+                imageUrl,
+                height: 70.0,
+                width: 70.0,
+              ),
+            ),
+            Row(
+              children: [
+                SvgPicture.asset('assets/images/fa-solid_user.svg'),
+                Text(
+                  numStudents,
+                  style: TextStyle(
+                    color: Color(0xFF575E83),
+                    fontFamily: 'Inter400',
+                    fontSize: 12.0,
+                  ),
+                ),
+                SizedBox(
+                  width: 63.0,
+                ),
+                SvgPicture.asset('assets/images/Star.svg'),
+                Text(
+                  '$rating',
+                  style: TextStyle(
+                    color: Color(0xFF575E83),
+                    fontFamily: 'Inter400',
+                    fontSize: 12.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
