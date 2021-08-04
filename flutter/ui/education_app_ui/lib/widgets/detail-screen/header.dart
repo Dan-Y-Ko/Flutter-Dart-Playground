63e0f093b282c8fb3912bd04c45de08eb99cc703
Dan Ko <danko56666@gmail.com>
2021-08-04 09:37:51 -0400

    add back button functionality

--- flutter/ui/education_app_ui/lib/widgets/detail-screen/header.dart
+++ flutter/ui/education_app_ui/lib/widgets/detail-screen/header.dart
@@ -11,31 +11,62 @@ class Header extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            height: 40.0,
-            width: 40.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: SvgPicture.asset('assets/images/left-arrow.svg'),
-            ),
+          Stack(
+            children: [
+              Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: SvgPicture.asset('assets/images/left-arrow.svg'),
+                ),
+              ),
+              Positioned.fill(
+                child: Material(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      10.0,
+                    ),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(
-            height: 40.0,
-            width: 40.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset('assets/images/header-bookmark.svg'),
-            ),
-          )
+          Bookmark()
         ],
+      ),
+    );
+  }
+}
+
+class Bookmark extends StatelessWidget {
+  const Bookmark({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.0,
+      width: 40.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SvgPicture.asset('assets/images/header-bookmark.svg'),
       ),
     );
   }
