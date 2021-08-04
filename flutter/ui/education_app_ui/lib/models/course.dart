43db2a3c165726d7bc04bb32aec7cb5cf5b1f464
Dan Ko <danko56666@gmail.com>
2021-08-04 04:43:51 -0400

    add color to course model

--- flutter/ui/education_app_ui/lib/models/course.dart
+++ flutter/ui/education_app_ui/lib/models/course.dart
@@ -4,6 +4,7 @@ class Course {
   final String imageUrl;
   final String numStudents;
   final double rating;
+  final String color;
 
   const Course({
     required this.title,
@@ -11,36 +12,37 @@ class Course {
     required this.imageUrl,
     required this.numStudents,
     required this.rating,
+    required this.color,
   });
 }
 
 const List<Course> courses = [
   Course(
-    title: 'Design',
-    numLecture: 54,
-    imageUrl: 'assets/images/Chart_perspective.jpg',
-    numStudents: '1.2k',
-    rating: 4.5,
-  ),
+      title: 'Design',
+      numLecture: 54,
+      imageUrl: 'assets/images/Chart_perspective.jpg',
+      numStudents: '1.2k',
+      rating: 4.5,
+      color: 'FEE2DE'),
   Course(
-    title: 'Grammar',
-    numLecture: 54,
-    imageUrl: 'assets/images/Books_perspective.jpg',
-    numStudents: '1.2k',
-    rating: 4.5,
-  ),
+      title: 'Grammar',
+      numLecture: 54,
+      imageUrl: 'assets/images/Books_perspective.jpg',
+      numStudents: '1.2k',
+      rating: 4.5,
+      color: 'FEDDEE'),
   Course(
-    title: 'Finance',
-    numLecture: 54,
-    imageUrl: 'assets/images/Dashboard_perspective.jpg',
-    numStudents: '1.2k',
-    rating: 4.5,
-  ),
+      title: 'Finance',
+      numLecture: 54,
+      imageUrl: 'assets/images/Dashboard_perspective.jpg',
+      numStudents: '1.2k',
+      rating: 4.5,
+      color: 'DAD5FE'),
   Course(
-    title: 'Coding',
-    numLecture: 54,
-    imageUrl: 'assets/images/Code_perspective.jpg',
-    numStudents: '1.2k',
-    rating: 4.5,
-  ),
+      title: 'Coding',
+      numLecture: 54,
+      imageUrl: 'assets/images/Code_perspective.jpg',
+      numStudents: '1.2k',
+      rating: 4.5,
+      color: 'DBFFD9'),
 ];
