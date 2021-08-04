894e5cb6181a3bc7e5556665ee31c7e74975505f
Dan Ko <danko56666@gmail.com>
2021-08-04 07:37:00 -0400

    split up widget

--- flutter/ui/education_app_ui/lib/widgets/detail-screen/info.dart
+++ flutter/ui/education_app_ui/lib/widgets/detail-screen/info.dart
@@ -18,69 +18,15 @@ class Info extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Basic Grammar',
-                  style: TextStyle(
-                    color: Color(0xFF4D2C5E),
-                    fontFamily: 'Inter700',
-                    fontSize: 30.0,
-                  ),
-                ),
+                Title(),
                 SizedBox(
                   height: 21.0,
                 ),
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/images/fa-solid_user.svg'),
-                    Text(
-                      '1.2k',
-                      style: TextStyle(
-                        color: Color(0xFF575E83),
-                        fontFamily: 'Inter400',
-                        fontSize: 15.75,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 60.0,
-                    ),
-                    SvgPicture.asset('assets/images/Star.svg'),
-                    Text(
-                      '4.5',
-                      style: TextStyle(
-                        color: Color(0xFF575E83),
-                        fontFamily: 'Inter400',
-                        fontSize: 15.75,
-                      ),
-                    ),
-                  ],
-                ),
+                Stats(),
                 SizedBox(
                   height: 21.0,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.ideographic,
-                  children: [
-                    Text(
-                      '\$280',
-                      style: TextStyle(
-                        color: Color(0xFF4D2C5E),
-                        fontFamily: 'Inter700',
-                        fontSize: 30.0,
-                      ),
-                    ),
-                    Text(
-                      '\$340',
-                      style: TextStyle(
-                        color: Color.fromRGBO(87, 94, 131, 0.5),
-                        fontFamily: 'Inter500',
-                        fontSize: 20.0,
-                        decoration: TextDecoration.lineThrough,
-                      ),
-                    ),
-                  ],
-                ),
+                Price(),
               ],
             ),
           ),
@@ -95,6 +41,93 @@ class Info extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Basic Grammar',
+      style: TextStyle(
+        color: Color(0xFF4D2C5E),
+        fontFamily: 'Inter700',
+        fontSize: 30.0,
+      ),
+    );
+  }
+}
+
+class Stats extends StatelessWidget {
+  const Stats({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset('assets/images/fa-solid_user.svg'),
+        Text(
+          '1.2k',
+          style: TextStyle(
+            color: Color(0xFF575E83),
+            fontFamily: 'Inter400',
+            fontSize: 15.75,
+          ),
+        ),
+        SizedBox(
+          width: 60.0,
+        ),
+        SvgPicture.asset('assets/images/Star.svg'),
+        Text(
+          '4.5',
+          style: TextStyle(
+            color: Color(0xFF575E83),
+            fontFamily: 'Inter400',
+            fontSize: 15.75,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Price extends StatelessWidget {
+  const Price({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.ideographic,
+      children: [
+        Text(
+          '\$280',
+          style: TextStyle(
+            color: Color(0xFF4D2C5E),
+            fontFamily: 'Inter700',
+            fontSize: 30.0,
+          ),
+        ),
+        Text(
+          '\$340',
+          style: TextStyle(
+            color: Color.fromRGBO(87, 94, 131, 0.5),
+            fontFamily: 'Inter500',
+            fontSize: 20.0,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+      ],
     );
   }
 }
