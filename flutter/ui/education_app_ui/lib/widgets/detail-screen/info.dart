eca251283da7f06623a4f7568907240886443629
Dan Ko <danko56666@gmail.com>
2021-08-04 07:34:53 -0400

    finish info section

--- flutter/ui/education_app_ui/lib/widgets/detail-screen/info.dart
+++ flutter/ui/education_app_ui/lib/widgets/detail-screen/info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Info extends StatelessWidget {
   const Info({Key? key}) : super(key: key);
@@ -10,30 +11,86 @@ class Info extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       child: Stack(
         children: [
-          // Container(
-          //   height: 171.0,
-          //   width: 142.0,
-          //   child: Column(
-          //     crossAxisAlignment: CrossAxisAlignment.start,
-          //     children: [
-          //       Text(
-          //         'Basic Grammar',
-          //         style: TextStyle(
-          //           color: Color(0xFF4D2C5E),
-          //           fontFamily: 'Inter700',
-          //           fontSize: 30.0,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
+          Container(
+            margin: EdgeInsets.only(left: 27.0),
+            height: 171.0,
+            width: 142.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Basic Grammar',
+                  style: TextStyle(
+                    color: Color(0xFF4D2C5E),
+                    fontFamily: 'Inter700',
+                    fontSize: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 21.0,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset('assets/images/fa-solid_user.svg'),
+                    Text(
+                      '1.2k',
+                      style: TextStyle(
+                        color: Color(0xFF575E83),
+                        fontFamily: 'Inter400',
+                        fontSize: 15.75,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 60.0,
+                    ),
+                    SvgPicture.asset('assets/images/Star.svg'),
+                    Text(
+                      '4.5',
+                      style: TextStyle(
+                        color: Color(0xFF575E83),
+                        fontFamily: 'Inter400',
+                        fontSize: 15.75,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 21.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.ideographic,
+                  children: [
+                    Text(
+                      '\$280',
+                      style: TextStyle(
+                        color: Color(0xFF4D2C5E),
+                        fontFamily: 'Inter700',
+                        fontSize: 30.0,
+                      ),
+                    ),
+                    Text(
+                      '\$340',
+                      style: TextStyle(
+                        color: Color.fromRGBO(87, 94, 131, 0.5),
+                        fontFamily: 'Inter500',
+                        fontSize: 20.0,
+                        decoration: TextDecoration.lineThrough,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
           Positioned(
-            left: 200,
+            top: -30.0,
+            left: 190.0,
             child: Image.asset(
               'assets/images/book.png',
-              height: 242.0,
+              height: 240.0,
               width: 242.0,
-              fit: BoxFit.cover,
             ),
           ),
         ],
