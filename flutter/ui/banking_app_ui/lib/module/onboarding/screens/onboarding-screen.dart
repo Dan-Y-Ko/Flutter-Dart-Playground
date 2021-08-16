aabe94ce470b8c2c489572ab706e15c7fee9524c
Dan Ko <danko56666@gmail.com>
2021-08-16 01:42:49 -0400

    implement everything up to progress indicator button

--- flutter/ui/banking_app_ui/lib/module/onboarding/screens/onboarding-screen.dart
+++ flutter/ui/banking_app_ui/lib/module/onboarding/screens/onboarding-screen.dart
@@ -1,3 +1,4 @@
+import 'package:banking_app_ui/module/onboarding/widgets/progress-indicator-button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -29,13 +30,93 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme().primaryBackground,
-      body: Center(
-        child: Text(
-          'Hello',
-          style: TextStyle(
-            color: AppTheme().primaryText,
-            fontSize: AppTheme().fontSizes[10],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 302.0,
+                height: 282.0,
+                child: Image.asset('assets/images/onboarding_bg_1.png'),
+              ),
+            ),
+            SizedBox(height: 79.0),
+            Text(
+              'Get Bank ID',
+              style: TextStyle(
+                fontWeight: AppTheme().fontWeights[2],
+                fontSize: AppTheme().fontSizes[10],
+                color: AppTheme().primaryText,
+              ),
+            ),
+            SizedBox(
+              height: 13.0,
+            ),
+            SizedBox(
+              width: 302.0,
+              child: Text(
+                'Our new service makes it easy for you to work anywhere, there are new features will ready help you.',
+                style: TextStyle(
+                  fontWeight: AppTheme().fontWeights[1],
+                  fontSize: AppTheme().fontSizes[6],
+                  color: AppTheme().primaryText,
+                  height: 1.7,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 20.0,
+                      height: 7.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        gradient: LinearGradient(
+                          colors: [
+                            AppTheme().primaryUI!,
+                            AppTheme().secondaryUI!,
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 4.0,
+                    ),
+                    Container(
+                      width: 11.0,
+                      height: 6.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: AppTheme().quaternaryUI,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 4.0,
+                    ),
+                    Container(
+                      width: 11.0,
+                      height: 6.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: AppTheme().quaternaryUI,
+                      ),
+                    ),
+                  ],
+                ),
+                ProgressIndicatorButton(),
+              ],
+            )
+          ],
         ),
       ),
     );
---
+++ flutter/ui/banking_app_ui/lib/module/onboarding/widgets/progress-indicator-button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorButton extends StatelessWidget {
+  const ProgressIndicatorButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const TWO_PI = 3.14 * 2;
+
+    return Container(
+      width: 80.0,
+      height: 80.0,
+      color: Colors.blue,
+    );
+  }
+}
