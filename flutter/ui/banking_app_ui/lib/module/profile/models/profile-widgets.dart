5ad1f981748fe19a67ac3433709241c900b99fd9
Dan Ko <danko56666@gmail.com>
2021-08-21 01:49:23 -0400

    finish profile screen

---
+++ flutter/ui/banking_app_ui/lib/module/profile/models/profile-widgets.dart
@@ -0,0 +1,49 @@
+import 'package:banking_app_ui/core/utils/theme/theme.dart';
+import 'package:banking_app_ui/module/profile/screens/profile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ProfileWidgets {
+  final String imageUrl;
+  final String title;
+  final Widget widget;
+
+  ProfileWidgets({
+    required this.imageUrl,
+    required this.title,
+    required this.widget,
+  });
+}
+
+List<ProfileWidgets> profileWidgets = [
+  ProfileWidgets(
+    imageUrl: 'assets/images/key.png',
+    title: 'Password',
+    widget: SvgPicture.asset('assets/images/caret_right.svg'),
+  ),
+  ProfileWidgets(
+    imageUrl: 'assets/images/touch.png',
+    title: 'Touch ID',
+    widget: Switch(
+      value: true,
+      onChanged: (bool) {},
+      activeColor: AppTheme().primaryText,
+      activeTrackColor: AppTheme().secondaryUI,
+    ),
+  ),
+  ProfileWidgets(
+    imageUrl: 'assets/images/language.png',
+    title: 'Language',
+    widget: SvgPicture.asset('assets/images/caret_right.svg'),
+  ),
+  ProfileWidgets(
+    imageUrl: 'assets/images/info.png',
+    title: 'App Information',
+    widget: SvgPicture.asset('assets/images/caret_right.svg'),
+  ),
+  ProfileWidgets(
+    imageUrl: 'assets/images/headphones.png',
+    title: 'Customer Care',
+    widget: SvgPicture.asset('assets/images/caret_right.svg'),
+  ),
+];
--- flutter/ui/banking_app_ui/lib/module/profile/screens/profile.dart
+++ flutter/ui/banking_app_ui/lib/module/profile/screens/profile.dart
@@ -1,5 +1,6 @@
 import 'package:banking_app_ui/core/widgets/header.dart';
 import 'package:banking_app_ui/module/auth/widgets/back-arrow-widget.dart';
+import 'package:banking_app_ui/module/profile/models/profile-widgets.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/utils/theme/theme.dart';
@@ -22,7 +23,56 @@ class Profile extends StatelessWidget {
                 Header(
                   leadingIcon: BackArrowButton(),
                   appBarText: 'Profile',
-                )
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/avatar_1.png'),
+                  radius: 52.0,
+                ),
+                SizedBox(
+                  height: 22.0,
+                ),
+                Text(
+                  'Kawsar Ahmed',
+                  style: TextStyle(
+                    color: AppTheme().secondaryText,
+                    fontSize: 29.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Kawsarui.ux@gmail.com',
+                  style: TextStyle(
+                    color: AppTheme().primaryText,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 53.0,
+                ),
+                for (var profileWidget in profileWidgets)
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: AppTheme().tertiaryUI,
+                    ),
+                    margin: EdgeInsets.only(bottom: 16.0),
+                    child: ListTile(
+                      leading: Image.asset(profileWidget.imageUrl),
+                      title: Text(
+                        profileWidget.title,
+                        style: TextStyle(
+                          color: AppTheme().primaryText,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      trailing: profileWidget.widget,
+                    ),
+                  ),
               ],
             ),
           ),
