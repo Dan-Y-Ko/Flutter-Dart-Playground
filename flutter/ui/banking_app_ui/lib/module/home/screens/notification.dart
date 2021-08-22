dcfd224ce7062a9dff408b677bddb0a9a108461c
Dan Ko <danko56666@gmail.com>
2021-08-22 00:08:53 -0400

    finish notification section

--- flutter/ui/banking_app_ui/lib/module/home/screens/notification.dart
+++ flutter/ui/banking_app_ui/lib/module/home/screens/notification.dart
@@ -1,6 +1,7 @@
 import 'package:banking_app_ui/core/utils/theme/theme.dart';
 import 'package:banking_app_ui/core/widgets/header.dart';
 import 'package:banking_app_ui/module/auth/widgets/back-arrow-widget.dart';
+import 'package:banking_app_ui/module/home/models/contacts.dart';
 import 'package:flutter/material.dart';
 
 class NotificationScreen extends StatelessWidget {
@@ -25,18 +26,83 @@ class NotificationScreen extends StatelessWidget {
                 height: 28.0,
               ),
               SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Today',
-                      style: TextStyle(
-                        color: AppTheme().primaryText,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w400,
+                child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Today',
+                        style: TextStyle(
+                          color: AppTheme().primaryText,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 17.0,
+                      ),
+                      for (Contacts contact in contactsList)
+                        Container(
+                          height: 70.0,
+                          margin: EdgeInsets.only(bottom: 17.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            color: AppTheme().tertiaryUI,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 19.0),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 18.0, bottom: 18.0),
+                                  child: CircleAvatar(
+                                    backgroundImage:
+                                        AssetImage(contact.avatarUrl),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 16.0,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    RichText(
+                                      text: TextSpan(
+                                        text: '${contact.name.split(' ')[0]} ',
+                                        style: TextStyle(
+                                          color: AppTheme().primaryText,
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text: 'started following you.',
+                                            style: TextStyle(
+                                              color: AppTheme().primaryText,
+                                              fontSize: 14.0,
+                                              fontWeight: FontWeight.w400,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Text(
+                                      '02 minutes ago.',
+                                      style: TextStyle(
+                                        color: AppTheme().primaryText,
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                    ],
+                  ),
                 ),
               ),
             ],
