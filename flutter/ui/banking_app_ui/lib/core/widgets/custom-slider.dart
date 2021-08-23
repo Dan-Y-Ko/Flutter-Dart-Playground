9556fe9a71873810f38f35894b46fdf1bd936bb8
Dan Ko <danko56666@gmail.com>
2021-08-23 03:44:49 -0400

    finish custom slider implementation

--- flutter/ui/banking_app_ui/lib/core/widgets/custom-slider.dart
+++ flutter/ui/banking_app_ui/lib/core/widgets/custom-slider.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:ui' as UI;
 
 import 'package:banking_app_ui/core/utils/theme/theme.dart';
@@ -36,6 +35,14 @@ class _CustomSliderState extends State<CustomSlider> {
           return SliderTheme(
             data: SliderTheme.of(context).copyWith(
               thumbShape: SliderThumbImage(snapshot.data),
+              trackHeight: 18.0,
+              trackShape: CustomTrackShape(
+                gradient: LinearGradient(
+                  colors: [AppTheme().secondaryUI!, AppTheme().primaryUI!],
+                ),
+              ),
+              overlayColor: Colors.transparent,
+              overlayShape: RoundSliderOverlayShape(overlayRadius: 0.0),
             ),
             child: Slider(
               value: sliderValue,
@@ -117,5 +124,104 @@ class SliderThumbImage extends SliderComponentShape {
       ..color = Colors.white;
 
     canvas.drawImage(image, imageOffset, paint);
+  }
+}
+
+class CustomTrackShape extends RoundedRectSliderTrackShape {
+  CustomTrackShape({
+    required this.gradient,
+  });
+
+  final LinearGradient gradient;
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset offset, {
+    required RenderBox parentBox,
+    required SliderThemeData sliderTheme,
+    required Animation<double> enableAnimation,
+    required TextDirection textDirection,
+    required Offset thumbCenter,
+    bool isDiscrete = false,
+    bool isEnabled = false,
+    double additionalActiveTrackHeight = 2,
+  }) {
+    // If the slider [SliderThemeData.trackHeight] is less than or equal to 0,
+    // then it makes no difference whether the track is painted or not,
+    // therefore the painting can be a no-op.
+    if (sliderTheme.trackHeight! <= 0) {
+      return;
+    }
+
+    final Rect trackRect = getPreferredRect(
+      parentBox: parentBox,
+      offset: offset,
+      sliderTheme: sliderTheme,
+      isEnabled: isEnabled,
+      isDiscrete: isDiscrete,
+    );
+
+    // Assign the track segment paints, which are left: active, right: inactive,
+    // but reversed for right to left text.
+    final ColorTween activeTrackColorTween = ColorTween(
+        begin: sliderTheme.disabledActiveTrackColor,
+        end: sliderTheme.activeTrackColor);
+
+    final ColorTween inactiveTrackColorTween = ColorTween(
+      begin: AppTheme().primaryBackground,
+      end: AppTheme().primaryBackground,
+    );
+
+    final Paint activePaint = Paint()
+      ..shader = gradient.createShader(trackRect)
+      ..color = activeTrackColorTween.evaluate(enableAnimation)!;
+
+    final Paint inactivePaint = Paint()
+      ..color = inactiveTrackColorTween.evaluate(enableAnimation)!;
+
+    final Paint leftTrackPaint;
+    final Paint rightTrackPaint;
+
+    switch (textDirection) {
+      case TextDirection.ltr:
+        leftTrackPaint = activePaint;
+        rightTrackPaint = inactivePaint;
+        break;
+      case TextDirection.rtl:
+        leftTrackPaint = inactivePaint;
+        rightTrackPaint = activePaint;
+        break;
+    }
+
+    final Rect leftTrackSegment = Rect.fromLTRB(
+      trackRect.left,
+      trackRect.top,
+      thumbCenter.dx,
+      trackRect.bottom,
+    );
+
+    final lRect = RRect.fromRectAndRadius(
+      leftTrackSegment,
+      Radius.circular(10.0),
+    );
+
+    if (!leftTrackSegment.isEmpty)
+      context.canvas.drawRRect(lRect, leftTrackPaint);
+
+    final Rect rightTrackSegment = Rect.fromLTRB(
+      thumbCenter.dx,
+      trackRect.top,
+      trackRect.right,
+      trackRect.bottom,
+    );
+
+    final rRect = RRect.fromRectAndRadius(
+      rightTrackSegment,
+      Radius.circular(10.0),
+    );
+
+    if (!rightTrackSegment.isEmpty)
+      context.canvas.drawRRect(rRect, rightTrackPaint);
   }
 }
