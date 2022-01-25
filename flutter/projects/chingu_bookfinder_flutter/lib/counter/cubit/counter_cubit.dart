68b4e00b1d779beb37c04dec29be5bd7439e5d1d
Dan Ko <danko56666@gmail.com>
2022-01-25 00:28:45 -0500

    test

--- flutter/projects/chingu_bookfinder_flutter/lib/counter/cubit/counter_cubit.dart
+++ flutter/projects/chingu_bookfinder_flutter/lib/counter/cubit/counter_cubit.dart
@@ -10,6 +10,6 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  void increment() => emit(state + 1);
+  void increment() => emit(state + 2);
   void decrement() => emit(state - 1);
 }
