acafde9dabd0a8306db10b7556c3f8cd50ad3a35
Dan Ko <danko56666@gmail.com>
2022-03-10 19:14:40 -0500

    create book model in google books api package

--- flutter/projects/chingu_bookfinder_flutter/packages/google_books_api/lib/src/models/book.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/google_books_api/lib/src/models/book.dart
@@ -1,1 +1,141 @@
-class Book {}
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'book.freezed.dart';
+part 'book.g.dart';
+
+@freezed
+class Book with _$Book {
+  factory Book({
+    String? kind,
+    String? id,
+    String? etag,
+    String? selfLink,
+    VolumeInfo? volumeInfo,
+    SaleInfo? saleInfo,
+    AccessInfo? accessInfo,
+    SearchInfo? searchInfo,
+  }) = _Book;
+
+  factory Book.fromJson(Map<String, dynamic> json) => _$BookFromJson(json);
+}
+
+@freezed
+class AccessInfo with _$AccessInfo {
+  factory AccessInfo({
+    String? country,
+    String? viewability,
+    bool? embeddable,
+    bool? publicDomain,
+    String? textToSpeechPermission,
+    Epub? epub,
+    Pdf? pdf,
+    String? webReaderLink,
+    String? accessViewStatus,
+    bool? quoteSharingAllowed,
+  }) = _AccessInfo;
+
+  factory AccessInfo.fromJson(Map<String, dynamic> json) =>
+      _$AccessInfoFromJson(json);
+}
+
+@freezed
+class Pdf with _$Pdf {
+  factory Pdf({
+    bool? isAvailable,
+  }) = _Pdf;
+
+  factory Pdf.fromJson(Map<String, dynamic> json) => _$PdfFromJson(json);
+}
+
+@freezed
+class Epub with _$Epub {
+  factory Epub({
+    bool? isAvailable,
+  }) = _Epub;
+
+  factory Epub.fromJson(Map<String, dynamic> json) => _$EpubFromJson(json);
+}
+
+@freezed
+class SaleInfo with _$SaleInfo {
+  factory SaleInfo({
+    String? country,
+    String? saleability,
+    bool? isEbook,
+  }) = _SaleInfo;
+
+  factory SaleInfo.fromJson(Map<String, dynamic> json) =>
+      _$SaleInfoFromJson(json);
+}
+
+@freezed
+class SearchInfo with _$SearchInfo {
+  factory SearchInfo({
+    String? textSnippet,
+  }) = _SearchInfo;
+
+  factory SearchInfo.fromJson(Map<String, dynamic> json) =>
+      _$SearchInfoFromJson(json);
+}
+
+@freezed
+class VolumeInfo with _$VolumeInfo {
+  factory VolumeInfo({
+    String? title,
+    List<String>? authors,
+    String? publisher,
+    String? publishedDate,
+    String? description,
+    List<IndustryIdentifier>? industryIdentifiers,
+    ReadingModes? readingModes,
+    int? pageCount,
+    String? printType,
+    List<String>? categories,
+    double? averageRating,
+    int? ratingsCount,
+    String? maturityRating,
+    bool? allowAnonLogging,
+    String? contentVersion,
+    ImageLinks? imageLinks,
+    String? language,
+    String? previewLink,
+    String? infoLink,
+    String? canonicalVolumeLink,
+  }) = _VolumeInfo;
+
+  factory VolumeInfo.fromJson(Map<String, dynamic> json) =>
+      _$VolumeInfoFromJson(json);
+}
+
+@freezed
+class ImageLinks with _$ImageLinks {
+  factory ImageLinks({
+    String? smallThumbnail,
+    String? thumbnail,
+  }) = _ImageLinks;
+
+  factory ImageLinks.fromJson(Map<String, dynamic> json) =>
+      _$ImageLinksFromJson(json);
+}
+
+@freezed
+class IndustryIdentifier with _$IndustryIdentifier {
+  factory IndustryIdentifier({
+    String? type,
+    String? identifier,
+  }) = _IndustryIdentifier;
+
+  factory IndustryIdentifier.fromJson(Map<String, dynamic> json) =>
+      _$IndustryIdentifierFromJson(json);
+}
+
+@freezed
+class ReadingModes with _$ReadingModes {
+  factory ReadingModes({
+    bool? text,
+    bool? image,
+  }) = _ReadingModes;
+
+  factory ReadingModes.fromJson(Map<String, dynamic> json) =>
+      _$ReadingModesFromJson(json);
+}
