21699ec738eca9a435a0185a6ad79822381be3f2
Dan Ko <danko56666@gmail.com>
2022-03-10 21:08:57 -0500

    implement get book method in google books api client

--- flutter/projects/chingu_bookfinder_flutter/packages/google_books_api/lib/src/google_books_api_client.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/google_books_api/lib/src/google_books_api_client.dart
@@ -4,10 +4,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import '../google_books_api.dart';
+import './models/book_volume.dart';
 
 class BookSearchRequestFailure implements Exception {}
 
 class BookNotFoundFailure implements Exception {}
+
+class BookVolumeRequestFailure implements Exception {}
+
+class BookVolumeNotFoundFailure implements Exception {}
 
 class GoogleBooksApiClient {
   GoogleBooksApiClient({
@@ -47,5 +52,28 @@ class GoogleBooksApiClient {
         .toList();
 
     return books;
+  }
+
+  Future<BookVolume> getBook(String volume) async {
+    final url = Uri.https(
+      _baseUrl,
+      '/books/v1/volumes/$volume',
+    );
+
+    final response = await _httpClient.get(
+      url,
+    );
+
+    if (response.statusCode != 200) {
+      throw BookVolumeRequestFailure();
+    }
+
+    final responseJson = jsonDecode(response.body) as Map<String, dynamic>;
+
+    if (responseJson.isEmpty) {
+      throw BookVolumeNotFoundFailure();
+    }
+
+    return BookVolume.fromJson(responseJson);
   }
 }
