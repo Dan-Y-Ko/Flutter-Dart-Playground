342499a0dcdef7f081f6efd7df9b27cbc2e4bd40
Dan Ko <danko56666@gmail.com>
2022-03-14 22:44:45 -0400

    setup initial boilerplate

--- flutter/projects/chingu_bookfinder_flutter/packages/base_api/lib/base_api.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/base_api/lib/base_api.dart
@@ -1,7 +1,3 @@
 library base_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/base_api.dart';
--- flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/book_repository.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/book_repository.dart
@@ -1,1 +1,6 @@
+import 'dart:async';
+
+import 'package:google_books_api/google_books_api.dart';
+import 'package:book_repository/book_repository.dart';
+
 class BookRepository {}
