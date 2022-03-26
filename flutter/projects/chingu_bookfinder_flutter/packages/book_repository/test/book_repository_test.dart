62e271ecc1749aea8e60c7d29dc9e05a4e0161df
Dan Ko <danko56666@gmail.com>
2022-03-26 05:11:10 -0400

    add unit tests for getBoook method

--- flutter/projects/chingu_bookfinder_flutter/packages/book_repository/test/book_repository_test.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/book_repository/test/book_repository_test.dart
@@ -1,5 +1,6 @@
 import 'package:base_api/base_api.dart' show AppException;
 import 'package:google_books_api/google_books_api.dart' as google_books_api;
+import 'package:google_books_api/src/models/book_volume.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -9,6 +10,8 @@ class MockGoogleApiClient extends Mock
     implements google_books_api.GoogleBooksApiClient {}
 
 class MockBook extends Mock implements google_books_api.Book {}
+
+class MockBookDetail extends Mock implements BookVolume {}
 
 void main() {
   group('Book Repository', () {
@@ -69,6 +72,55 @@ void main() {
 
         expect(
           () async => bookRepository.getBooks(''),
+          throwsA(
+            isA<AppException>(),
+          ),
+        );
+      });
+    });
+
+    group('getBook', () {
+      test('returns correct Book model', () async {
+        final book = MockBookDetail();
+        when(() => book.id).thenReturn('kLAoswEACAAJ');
+        when(() => book.volumeInfo).thenReturn(
+          VolumeInfo(
+            imageLinks: ImageLinks(
+              thumbnail:
+                  'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+            ),
+            authors: ['J. K. Rowling', 'Jack Thorne', 'John Tiffany'],
+            publisher: 'Sphere',
+            title: 'Harry Potter and the Cursed Child',
+            description: 'some description',
+          ),
+        );
+        when(() => googleBooksApiClient.getBook(any()))
+            .thenAnswer((_) async => book);
+
+        final actual = await bookRepository.getBook('kLAoswEACAAJ');
+
+        expect(
+          actual,
+          BookDetail(
+            id: 'kLAoswEACAAJ',
+            thumbnail:
+                'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+            authors: ['J. K. Rowling', 'Jack Thorne', 'John Tiffany'],
+            publisher: 'Sphere',
+            title: 'Harry Potter and the Cursed Child',
+            description: 'some description',
+          ),
+        );
+      });
+
+      test('properly throws when there is exception', () {
+        when(() => googleBooksApiClient.getBook(any())).thenThrow(
+          AppException('No Internet Connection'),
+        );
+
+        expect(
+          () async => bookRepository.getBook(''),
           throwsA(
             isA<AppException>(),
           ),
