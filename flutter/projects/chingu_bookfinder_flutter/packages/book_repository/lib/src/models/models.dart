c7cad32f80577f65ca5ee71ee3379032a8876caa
Dan Ko <danko56666@gmail.com>
2022-03-26 04:35:35 -0400

    add unit tests for book detail

--- flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/models/models.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/models/models.dart
@@ -1,1 +1,2 @@
 export 'book.dart';
+export 'book_detail.dart';
--- flutter/projects/chingu_bookfinder_flutter/packages/book_repository/test/book_detail_test.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/book_repository/test/book_detail_test.dart
@@ -4,36 +4,39 @@ import 'package:book_repository/book_repository.dart';
 
 void main() {
   group('Book model', () {
-    late Book _book;
+    late BookDetail _bookDetail;
 
     setUp(() {
-      _book = Book(
+      _bookDetail = BookDetail(
         id: 'kLAoswEACAAJ',
         thumbnail:
             'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
         authors: ['J. K. Rowling', 'Jack Thorne', 'John Tiffany'],
         publisher: 'Sphere',
         title: 'Harry Potter and the Cursed Child',
+        description: 'some description',
       );
     });
 
     test('instantiates properly', () {
-      expect(_book.id, 'kLAoswEACAAJ');
-      expect(_book.thumbnail,
+      expect(_bookDetail.id, 'kLAoswEACAAJ');
+      expect(_bookDetail.thumbnail,
           'http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api');
-      expect(_book.authors, ['J. K. Rowling', 'Jack Thorne', 'John Tiffany']);
-      expect(_book.publisher, 'Sphere');
-      expect(_book.title, 'Harry Potter and the Cursed Child');
+      expect(_bookDetail.authors,
+          ['J. K. Rowling', 'Jack Thorne', 'John Tiffany']);
+      expect(_bookDetail.publisher, 'Sphere');
+      expect(_bookDetail.title, 'Harry Potter and the Cursed Child');
+      expect(_bookDetail.description, 'some description');
     });
 
     test('object comparison is supported', () {
-      expect(_book, equals(_book));
+      expect(_bookDetail, equals(_bookDetail));
     });
 
     test('toString returns expected string', () {
       expect(
-        _book.toString(),
-        'Book(kLAoswEACAAJ, http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api, [J. K. Rowling, Jack Thorne, John Tiffany], Sphere, Harry Potter and the Cursed Child)',
+        _bookDetail.toString(),
+        'BookDetail(kLAoswEACAAJ, http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api, [J. K. Rowling, Jack Thorne, John Tiffany], some description, Sphere, Harry Potter and the Cursed Child)',
       );
     });
   });
