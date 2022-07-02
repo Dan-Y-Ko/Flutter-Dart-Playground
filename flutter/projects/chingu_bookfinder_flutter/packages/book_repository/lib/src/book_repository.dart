211889ca32899eb39c640a8735beb5d3903f007e
Dan Ko <danko56666@gmail.com>
2022-07-02 09:47:50 -0400

    Decode HTML encoded text

--- flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/book_repository.dart
+++ flutter/projects/chingu_bookfinder_flutter/packages/book_repository/lib/src/book_repository.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:base_api/base_api.dart';
+import 'package:html/parser.dart' show parse;
 import 'package:google_books_api/google_books_api.dart' hide Book;
 import 'package:book_repository/book_repository.dart';
 
@@ -45,7 +46,7 @@ class BookRepository {
         thumbnail: book.volumeInfo!.imageLinks?.thumbnail ??
             'https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png',
         authors: book.volumeInfo?.authors ?? [],
-        description: book.volumeInfo!.description!,
+        description: parse(book.volumeInfo!.description!).body!.text,
         publisher: book.volumeInfo!.publisher ?? '',
         title: book.volumeInfo!.title ?? '',
       );
