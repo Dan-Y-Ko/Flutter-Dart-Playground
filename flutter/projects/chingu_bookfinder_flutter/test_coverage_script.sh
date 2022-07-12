b2156cd454437f3f392001628b8ab60671877faf
Dan Ko <danko56666@gmail.com>
2022-07-11 23:20:50 -0400

    remove flutter from base api dependency

--- flutter/projects/chingu_bookfinder_flutter/test_coverage_script.sh
+++ flutter/projects/chingu_bookfinder_flutter/test_coverage_script.sh
@@ -1,3 +1,13 @@
 flutter test --coverage
 genhtml coverage/lcov.info -o coverage
 open coverage/index.html
+
+cd packages/base_api
+flutter test --coverage
+genhtml coverage/lcov.info -o coverage
+open coverage/index.html
+
+cd ../google_books_api
+flutter test --coverage
+genhtml coverage/lcov.info -o coverage
+open coverage/index.html
