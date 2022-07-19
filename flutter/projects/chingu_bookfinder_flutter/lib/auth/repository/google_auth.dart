560b302c80d4a755622ad408dc63fd478698d437
Dan Ko <danko56666@gmail.com>
2022-07-18 21:53:39 -0400

    implement google auth repository

---
+++ flutter/projects/chingu_bookfinder_flutter/lib/auth/repository/google_auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthRepository {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
