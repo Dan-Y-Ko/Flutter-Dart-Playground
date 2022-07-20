880f349606ef282f51e940017ba4a934dbf66e9e
Dan Ko <danko56666@gmail.com>
2022-07-20 15:55:04 -0400

    add unit test for firebase error cases

---
+++ flutter/projects/chingu_bookfinder_flutter/packages/firebase_authentication/test/firebase_error_test.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_authentication/firebase_authentication.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('SignInWithGoogleFailure', () {
+    const firebaseErrorCases = [
+      'account-exists-with-different-credential',
+      'invalid-credential',
+      'operation-not-allowed',
+      'user-disabled',
+      'user-not-found',
+      'wrong-password',
+      'invalid-verification-code',
+      'invalid-verification-id',
+    ];
+
+    for (String input in firebaseErrorCases) {
+      test("all error cases covered", () {
+        expect(SignInWithGoogleFailure.fromCode(input),
+            isA<SignInWithGoogleFailure>());
+      });
+    }
+  });
+}
