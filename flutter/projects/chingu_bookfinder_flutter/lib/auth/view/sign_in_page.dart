6eb349b9d8ceb40bdb402bec6a4071f3c08b79b5
Dan Ko <danko56666@gmail.com>
2022-07-27 21:26:36 -0400

    create custom background for login screen with custom paint

--- flutter/projects/chingu_bookfinder_flutter/lib/auth/view/sign_in_page.dart
+++ flutter/projects/chingu_bookfinder_flutter/lib/auth/view/sign_in_page.dart
@@ -8,40 +8,61 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  context.read<GoogleAuthBloc>().add(
-                        GoogleSignInEvent(),
-                      );
-
-                  context.go('/book');
-                },
-                child: BlocBuilder<GoogleAuthBloc, GoogleAuthState>(
-                  builder: (context, state) {
-                    if (state.status == GoogleAuthStatus.loading) {
-                      return const Padding(
-                        padding: EdgeInsets.all(10),
-                        child: CircularProgressIndicator(
-                          color: Colors.white,
-                        ),
-                      );
-                    }
-
-                    return const Text('Sign in with google');
-                  },
-                ),
-              ),
-            ],
-          ),
+        child: CustomPaint(
+          size: Size(width, height),
+          painter: CustomBackground(),
         ),
       ),
     );
+  }
+}
+
+class CustomBackground extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Offset.zero & size;
+
+    final paint = Paint()
+      ..shader = const LinearGradient(
+        colors: [
+          Color(0xFF23d9ac),
+          Color(0xFF00ff08),
+        ],
+      ).createShader(rect)
+      ..style = PaintingStyle.fill;
+
+    final path = Path()
+      ..lineTo(0, size.height * .3)
+      ..quadraticBezierTo(
+        size.width * .6,
+        size.height * .4,
+        size.width,
+        size.height * 0.2,
+      )
+      ..lineTo(size.width, 0)
+      ..close();
+
+    final path2 = Path()
+      ..moveTo(size.width, size.height * .4)
+      ..quadraticBezierTo(
+        size.width * 0.5,
+        size.height * .6,
+        size.width * .9,
+        size.height,
+      )
+      ..lineTo(size.width, size.height);
+
+    path.addPath(path2, Offset.zero);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
