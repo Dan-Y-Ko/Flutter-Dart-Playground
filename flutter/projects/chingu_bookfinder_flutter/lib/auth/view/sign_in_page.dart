c8294500624a269668b51c3afab39de71e6ee6c0
Dan Ko <danko56666@gmail.com>
2022-07-28 22:28:35 -0400

    move custom background to separate widget

--- flutter/projects/chingu_bookfinder_flutter/lib/auth/view/sign_in_page.dart
+++ flutter/projects/chingu_bookfinder_flutter/lib/auth/view/sign_in_page.dart
@@ -8,61 +8,10 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
-
     return Scaffold(
       body: SafeArea(
-        child: CustomPaint(
-          size: Size(width, height),
-          painter: CustomBackground(),
-        ),
+        child: CustomBackground(),
       ),
     );
-  }
-}
-
-class CustomBackground extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final rect = Offset.zero & size;
-
-    final paint = Paint()
-      ..shader = const LinearGradient(
-        colors: [
-          Color(0xFF23d9ac),
-          Color(0xFF00ff08),
-        ],
-      ).createShader(rect)
-      ..style = PaintingStyle.fill;
-
-    final path = Path()
-      ..lineTo(0, size.height * .3)
-      ..quadraticBezierTo(
-        size.width * .6,
-        size.height * .4,
-        size.width,
-        size.height * 0.2,
-      )
-      ..lineTo(size.width, 0)
-      ..close();
-
-    final path2 = Path()
-      ..moveTo(size.width, size.height * .4)
-      ..quadraticBezierTo(
-        size.width * 0.5,
-        size.height * .6,
-        size.width * .9,
-        size.height,
-      )
-      ..lineTo(size.width, size.height);
-
-    path.addPath(path2, Offset.zero);
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
--- flutter/projects/chingu_bookfinder_flutter/lib/auth/widgets/widgets.dart
+++ flutter/projects/chingu_bookfinder_flutter/lib/auth/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'confirm_password_input.dart';
+export 'custom_background.dart';
 export 'email_input.dart';
 export 'google_signin_button.dart';
 export 'password_input.dart';
